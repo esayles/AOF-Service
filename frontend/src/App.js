3f7cdfe6c36013591fd94f4d0906fb9cf73c3108
DaQUENCHIEST <119827276+DaQUENCHIEST@users.noreply.github.com>
2025-11-24 10:42:02 -0500

    Updated welcome message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,10 @@ import React from 'react';
 function App() {
   return (
     <div style={{ padding: '20px', fontFamily: 'Arial' }}>
-      <h1>AOF Service</h1>
+      <h1>AOF Service Hour Tracker</h1>
       <p>Frontend deployed successfully! 🦫🪽</p>
       <p>Hello World. Zach was her :)e</p>
+      <p>Hello Zach. World was here :[</p>
     </div>
   );
 }
