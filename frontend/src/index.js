583dbe4860457796d00858c262736fc7960b0906
DaQUENCHIEST <119827276+DaQUENCHIEST@users.noreply.github.com>
2026-03-16 18:08:47 -0500

    Adds a menu bar
    The menu bar allows for the user to switch between pages. As we have only the form at the moment, the links will be implemented in the future.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+//creates the root elemen for HTML, and prepares it for react 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//Renders the app into the router so that the links in the menu bar work, and the app is displayed on the page
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
