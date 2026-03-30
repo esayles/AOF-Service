dc4e98fc5a25def6ed0b284e38c003916b4770eb
DaQUENCHIEST <119827276+DaQUENCHIEST@users.noreply.github.com>
2026-03-30 11:15:03 -0400

    JTW change

--- frontend/src/Services/LeaderboardService.js
+++ frontend/src/Services/LeaderboardService.js
@@ -1,33 +1,34 @@
 //file fetches the data from the data base then creates a new array which it then sorts in decending order 
 
 export async function fetchLeaderboard() {
-    const token = localStorage.getItem("token");
+
+  const token = localStorage.getItem("access", data.access);
+
     
-    if (!token) {
-        throw new Error("No auth token found. Please log in.");
-      }
-
-    const response = await fetch('http://localhost:8000/api/leaderboard/', {
-        method: "GET",
-        headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Token ${token}`,
-    },
-  });
-    //debugging 
-    console.log("response:", response);
-
-    // Check if the response is unsuccessful
-    if(!response.ok) {
-        const errorText = await response.text(); // Get the error message from backend
-        //no idea how this line works VS code auto filled for me, but it works so hey. 
-        throw new Error(`Failed to fetch leaderboard: ${response.status}: ${errorText}`);
+  if (!token) {
+      throw new Error("No auth token found. Please log in.");
     }
 
-    const data = await response.json();
-    //debugging
-    console.log("Fetched leaderboard:", data);
+    const response = await fetch("http://localhost:8000/api/dashboard/", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+  //debugging 
+  console.log("response:", response);
 
-    const sortedStuents = [...data].sort((a, b) => b.hours - a.hours); // Sort by hours in descending order
-    return sortedStuents
+  // Check if the response is unsuccessful
+  if(!response.ok) {
+      const errorText = await response.text(); // Get the error message from backend
+      //no idea how this line works VS code auto filled for me, but it works so hey. 
+      throw new Error(`Failed to fetch leaderboard: ${response.status}: ${errorText}`);
+    }
+
+  const data = await response.json();
+  //debugging
+  console.log("Fetched leaderboard:", data);
+
+  const sortedStuents = [...data].sort((a, b) => b.hours - a.hours); // Sort by hours in descending order
+  return sortedStuents
 }
