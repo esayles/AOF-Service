0cae9883dbcfd1722c8cc4e957fd951b67dbd8d8
zcoder59 <212020557+zcoder59@users.noreply.github.com>
2026-04-07 14:43:16 -0400

    Makes the Dashboard Phone friendly

--- frontend/src/components/StudentDashboard.js
+++ frontend/src/components/StudentDashboard.js
@@ -1,17 +1,18 @@
 import react from 'react';
+
 import Leaderboard from "./Leaderboard";
 import ServiceLogForm from './ServiceLogForm';
 
 
 function StudentDashboard() {
-    return (             
-        <div class="container overflow-hidden text-center">
-            <div class="row gy-5">
-                <div class="col-6">
-                    <div class="p-3"><ServiceLogForm></ServiceLogForm></div>
+    return (
+        <div className="container overflow-hidden text-center">
+            <div className="row gy-5">
+                <div className="col-lg-6">
+                    <div className="p-3"><ServiceLogForm /></div>
                 </div>
-                <div class="col-6">
-                    <div class="p-3"><Leaderboard></Leaderboard></div>
+                <div className="col-lg-6">
+                    <div className="p-3"><Leaderboard /></div>
                 </div>
             </div>
         </div>
